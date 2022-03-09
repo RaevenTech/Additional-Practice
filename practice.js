f949332124bc0a125eaef86c83f8b9af378e216c
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-03-09 15:44:55 +0000

    Update practice.js

--- practice.js
+++ practice.js
@@ -23,23 +23,22 @@ function golfScore(par, strokes) {
 console.log(golfScore(5, 2));
 
 // Golf score funtion 2
-function golfScoreTwo(par, strokes) {
-    if (strokes == 1) {
+function golfScoreTwo(par1, strokes1) {
+    if (strokes1 == 1) {
         return names[0];
-    } else if (strokes <= par - 2) {
+    } else if (strokes1 <= par1 - 2) {
         return names[1];
-    } else if (strokes === par - 1) {
+    } else if (strokes1 === par1 - 1) {
         return names[2];
-    } else if (strokes === par) {
+    } else if (strokes1 === par1) {
         return names[3];
-    } else if (strokes === par + 1) {
+    } else if (strokes1 === par1 + 1) {
         return names[4];
-    } else if (strokes === par + 2) {
+    } else if (strokes1 === par1 + 2) {
         return names[5];
-    } else {
+    } else if (strokes1 >= par1 + 3) {
         return names[6];
     }
 
 }
 
-console.log(golfScoreTwo(5, 4);
